3e26bfc36eb296fd1658864a47c6b45b9170a296
Rares Cristea <cristea.rares96@gmail.com>
2023-11-03 14:45:31 +0200

    Hotfix for last commit

---
+++ vocea-universitatii/Models/Department.cs
@@ -0,0 +1,14 @@
+﻿using vocea_universitatii.Controllers;
+
+namespace vocea_universitatii.Models;
+
+public class Department : BaseModel
+{
+    public long Id { get; set; }
+    
+    public String FullName { get; set; }
+    
+    public long FacultyId { get; set; }
+    
+    public Faculty Faculty { get; set; }
+}
