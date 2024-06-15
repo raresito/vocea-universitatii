1c1c6f660ff29660b0b3560541ef322df31d2bae
Rares Cristea <cristea.rares96@gmail.com>
2024-06-15 20:03:30 +0300

    Improved readability in Forms Migration

--- LegacyDatabaseMigration/FormsMigration.cs
+++ LegacyDatabaseMigration/FormsMigration.cs
@@ -2,11 +2,11 @@ using System.Diagnostics;
 using System.Text.Json;
 using LegacyDatabaseMigration.CourseEvakData;
 using LegacyDatabaseMigration.CourseEvalModels.CourseEvalFormModels;
+using Microsoft.EntityFrameworkCore;
 using VoceaUniversitatiiDataModels;
 using VoceaUniversitatiiDataModels.Models;
-using Form = LegacyDatabaseMigration.CourseEvalModels.Form;
-
-namespace HttpClientSample;
+using LegacyForm = LegacyDatabaseMigration.CourseEvalModels.Form;
+using VoceaForm = VoceaUniversitatiiDataModels.Models.Form;
 
 public class FormsMigration
 {
@@ -19,58 +19,108 @@ public class FormsMigration
         _target = target;
     }
     
-    public void Migrate()
+    public List<LegacyForm> CollectLegacyForms()
     {
-        Form legacyForm = _source.Forms.First();
-            VoceaUniversitatiiDataModels.Models.Form newForm = new VoceaUniversitatiiDataModels.Models.Form();
-            newForm.FormName = legacyForm.Title;
-            newForm.CreatedAt = legacyForm.CreatedAt;
-            newForm.Id = legacyForm.Id;
-            
-            LegacyFormConentData legacyFormContentData = JsonSerializer.Deserialize<LegacyFormConentData>(legacyForm.Content);
-            
-            int formQuestionsCounter = 1;
-            
-            foreach (var legacySectionList in legacyFormContentData.Chestionar)
+        return _source.Forms.ToList();
+    }
+    
+    public VoceaForm CreateNewForm(LegacyForm legacyForm)
+    {
+        return new VoceaForm
+        {
+            FormName = legacyForm.Title,
+            CreatedAt = legacyForm.CreatedAt,
+            Id = legacyForm.Id
+        };
+    }
+    
+    public LegacyFormContentData DeserializeLegacyFormContent(string content)
+    {
+        return JsonSerializer.Deserialize<LegacyFormContentData>(content);
+    }
+    
+    public void ProcessLegacySectionList(List<LegacyQuestionSection> legacySectionList, VoceaForm newForm, ref int formQuestionsCounter)
+    {
+        foreach (var legacyQuestionSection in legacySectionList)
+        {
+            Question question = new Question();
+            question.LegacyFormOrderNumber = formQuestionsCounter;
+            ProcessLegacyQuestionSection(legacyQuestionSection, question);
+            if (newForm.Questions is null)
+                newForm.Questions = new List<Question>();
+            newForm.Questions.Add(question);
+            formQuestionsCounter++;
+        }
+    }
+    
+    public void ProcessLegacyQuestionSection(LegacyQuestionSection legacyQuestionSection, Question question)
+    {
+        if (legacyQuestionSection.Intrebare is not null)
+        {
+            foreach (var questionOrLabel in legacyQuestionSection.Intrebare)
             {
-                foreach (var legacyQuestionSection in legacySectionList)
+                if (questionOrLabel.Label is null && questionOrLabel.Rasp is null &&
+                    questionOrLabel.Enunt is not null)
                 {
-                    Question question = new Question();
-                    question.LegacyFormOrderNumber = formQuestionsCounter;
-                    if (legacyQuestionSection.Intrebare is not null)
+                    question.Title = questionOrLabel.Enunt;
+                }
+                else if (questionOrLabel.Label is null && questionOrLabel.Rasp is not null &&
+                         questionOrLabel.Enunt is null)
+                {
+                    QuestionOptions questionOptions = new QuestionOptions
                     {
-                        foreach (var questionOrLabel in legacyQuestionSection.Intrebare)
-                        {
-                            if (questionOrLabel.Label is null && questionOrLabel.Rasp is null &&
-                                questionOrLabel.Enunt is not null)
-                            {
-                                question.Title = questionOrLabel.Enunt;
-                            }
-                            else if (questionOrLabel.Label is null && questionOrLabel.Rasp is not null &&
-                                     questionOrLabel.Enunt is null)
-                            {
-                                QuestionOptions questionOptions = new QuestionOptions();
-                                questionOptions.OptionText = questionOrLabel.Rasp;
-                                questionOptions.Question = question;
-                                if (question.QuestionOptions is null)
-                                    question.QuestionOptions = new List<QuestionOptions>();
-                                question.QuestionOptions.Add(questionOptions);
-                                question.QuestionType = QuestionType.Options;
-                            }
-                            else
-                            {
-                                Console.WriteLine("Invalid question or label");
-                                Debugger.Launch();
-                            }
-                        }
-                        if(question.QuestionOptions is null)
-                            question.QuestionType = QuestionType.Text;
-                        if (newForm.Questions is null)
-                            newForm.Questions = new List<Question>();
-                        newForm.Questions.Add(question);
-                        formQuestionsCounter++;
-                    }
+                        OptionText = questionOrLabel.Rasp,
+                        Question = question
+                    };
+                    if (question.QuestionOptions is null)
+                        question.QuestionOptions = new List<QuestionOptions>();
+                    question.QuestionOptions.Add(questionOptions);
+                    question.QuestionType = QuestionType.Options;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid question or label");
+                    Debugger.Launch();
                 }
             }
+            if(question.QuestionOptions is null)
+                question.QuestionType = QuestionType.Text;
+        }
+    }
+    
+
+    public VoceaForm BuildVoceaFormInMemory(LegacyForm legacyForm)
+    {
+        VoceaForm newForm = CreateNewForm(legacyForm);
+        LegacyFormContentData legacyFormContentData = DeserializeLegacyFormContent(legacyForm.Content);
+        int formQuestionsCounter = 1;
+        
+        foreach (var legacySectionList in legacyFormContentData.Chestionar)
+        {
+            ProcessLegacySectionList(legacySectionList, newForm, ref formQuestionsCounter);
+        }
+
+        return newForm;
+    }
+    
+    public void SaveVoceaFormToDatabase(VoceaForm newForm)
+    {
+        //Set default language - Romanian
+        newForm.LanguageId = 4;
+    }
+    
+    public void Migrate()
+    {
+        List<LegacyForm> legacyForms = CollectLegacyForms();
+        List<VoceaForm> newForms = new List<VoceaForm>();
+        foreach (var legacyForm in legacyForms)
+        {
+            VoceaForm newForm = BuildVoceaFormInMemory(legacyForm);
+            newForms.Add(newForm);
+        }
+        foreach(var newForm in newForms)
+        {
+            SaveVoceaFormToDatabase(newForm);
+        }
     }
 }
